5adc5ab2d4109e09023cc9dec04026a49606b602
KayliedeBlois <kayliedeblois@gmail.com>
2022-11-10 12:33:02 -0500

    String literals

---
+++ strings.py
@@ -0,0 +1,22 @@
+firstLine = "Hey Jude"
+secondLine = "Don't make it bad"
+thirdLine = "Take a sad song"
+fourthLine = "And make it better"
+
+print(firstLine)
+print(secondLine)
+print(thirdLine)
+print(fourthLine)
+
+# use \n to create new line
+
+validString = "Hey Jude\nDon't make it bad\nTake a sad song\nAnd make it better"
+print(validString)
+
+# use triple quotes around multi line strings
+
+veryValidString = """Hey Jude
+Don't make it bad
+Take a sad song
+And make it better"""
+print(veryValidString)
